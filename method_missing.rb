26e19312c73e0f21bb761a48fd50e224b45054ed
ZolotarevArtem <Rebelition1@gmail.com>
2013-11-14 00:16:21 +0700

    fix search

--- method_missing.rb
+++ method_missing.rb
@@ -12,36 +12,36 @@ cars = [
     title: "Mazda rx-7",
     engine: 1300,
     length: 4285,
-		width: 1760,
-		height: 1230,
-		weight: 1280,
+    width: 1760,
+    height: 1230,
+    weight: 1280,
   },
 
   {
-		title: "Toyota Supra",
-		engine: 1298,
-		length: 4520,
-		width: 1810,
-		height: 1275,
-		weight: 1620,
+    title: "Toyota Supra",
+    engine: 1298,
+    length: 4520,
+    width: 1810,
+    height: 1275,
+    weight: 1620,
   },
 
   {
-		title: "Bugatti Veyron",
-		engine: 7993,
-		length: 4462,
-		width: 1998,
-		height: 1159,
-		weight: 1838,
+    title: "Bugatti Veyron",
+    engine: 7993,
+    length: 4462,
+    width: 1998,
+    height: 1159,
+    weight: 1838,
   },
 
   {
-		title: "Mercedes Benz",
-		engine: 5900,
-		length: 4902,
-		width: 1872,
-		height: 1420,
-		weight: 1870,
+    title: "Mercedes Benz",
+    engine: 5900,
+    length: 4902,
+    width: 1872,
+    height: 1420,
+    weight: 1870,
   }
 ]
 
@@ -50,13 +50,13 @@ class Sorter
   def method_missing(m, args)
     if m =~ /^by_/
       search = m.to_s.sub(/^by_/, '')
-	    sortCar = args.sort_by{ |value| value[search.to_sym] }
-			for i in 0...sortCar.length
-		  	puts sortCar[i][:title] + "\t#{sortCar[i][search.to_sym]}"
-			end
-			else
-		  	puts "not found: #{m}"
-		end
+      sortCar = args.sort_by{ |value| value[search.to_sym] }
+      for i in 0...sortCar.length
+        puts sortCar[i][:title] + "\t#{sortCar[i][search.to_sym]}"
+      end
+      else
+        puts "not found: #{m}"
+      end
   end
 
 end
